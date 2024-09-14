55c2169c4a27d2bc867bc1e4739ca63c7bb2cc52
sameer-uprety <sameeruprety321@gmail.com>
2024-09-14 11:18:39 +0530

    Updated sql and readme files

--- SQL/stakeholder_questions.sql
+++ SQL/stakeholder_questions.sql
@@ -9,6 +9,8 @@ select * from
     JOIN customers c ON t.customer_code = c.customer_code
     GROUP BY m.zone, c.custmer_name) ranked_customers
     where ranked <=5;
+-- See the query output here: https://docs.google.com/spreadsheets/d/1obs31VzcFcczj1aFsfJLVuldW1XllStzDCW5eW7_B2M/edit?usp=sharing
+
 -- Q2.Least sold market by zones
 select * from
 (select m.zone, m.markets_name, sum(t.sales_amount) as total_sales,
@@ -19,6 +21,9 @@ from markets m inner join transactions t on m.markets_code = t.market_code
 group by m.zone, m.markets_name
 ) ranked_by_sales
 where ranked = 1;
+
+-- See the query output here: https://docs.google.com/spreadsheets/d/1ZjlNuDfF8JUpgW_1UKB6B5nABw150_lnKIrWaKHxUGI/edit?usp=sharing
+
 -- Q3. Least sold product by year
 select Sale_Year, product_code, Total_Sales from
 (
@@ -31,4 +36,5 @@ group by Sale_Year, t.product_code
 order by Sale_Year,Total_Sales) ranked_products
 where ranked = 1;
 
+-- See the qery output here: https://docs.google.com/spreadsheets/d/1LQZxuB_d4T5evMt5c01mKjQixhOdbwgKJVdFkh-7pjM/edit?usp=sharing
 
